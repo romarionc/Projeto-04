d45ef4f60a07cc4874cbc3fb4607042492e981c4
romarionc <nc.romario@gmail.com>
2025-10-30 19:59:56 -0300

    feat: projetos-data foi editado para carregar os webp

--- js/projetos-data.js
+++ js/projetos-data.js
@@ -4,45 +4,45 @@ export const dadosProjetos = {
   titulo: "Nossos Projetos em Ação",
   imagensCarousel: [
     {
-      src: "img/doacao-01.jpg",
+      src: "img/doacao-01.webp",
       alt: "Voluntários montando cestas básicas para doação.",
     },
     {
-      src: "img/doacao-02.jpg",
+      src: "img/doacao-02.webp",
       alt: "Pessoa recebendo um prato de sopa quente.",
     },
     {
-      src: "img/doacao-03.jpg",
+      src: "img/doacao-03.webp",
       alt: "Caixa de papelão repleta de alimentos não perecíveis.",
     },
-    { src: "img/doacao-04.jpg", alt: "Criança comendo uma maçã." },
+    { src: "img/doacao-04.webp", alt: "Criança comendo uma maçã." },
     {
-      src: "img/doacao-05.jpg",
+      src: "img/doacao-05.webp",
       alt: "Distribuição de sacolas de alimentos para a comunidade.",
     },
     {
-      src: "img/doacao-06.jpg",
+      src: "img/doacao-06.webp",
       alt: "Agricultor doando vegetais frescos para a ONG.",
     },
     {
-      src: "img/doacao-07.jpg",
+      src: "img/doacao-07.webp",
       alt: "Veículo da ONG sendo carregado com doações de alimentos.",
     },
     {
-      src: "img/doacao-08.jpg",
+      src: "img/doacao-08.webp",
       alt: "Armazém da ONG com prateleiras cheias de alimentos.",
     },
     {
-      src: "img/doacao-09.jpg",
+      src: "img/doacao-09.webp",
       alt: "Equipe de voluntários da 'Comida para Todos'.",
     },
     {
-      src: "img/doacao-10.jpg",
+      src: "img/doacao-10.webp",
       alt: "Pessoa recebendo uma cesta básica de um voluntário.",
     },
-    { src: "img/doacao-11.jpg", alt: "Arroz e feijão para doação." },
+    { src: "img/doacao-11.webp", alt: "Arroz e feijão para doação." },
     {
-      src: "img/doacao-12.jpg",
+      src: "img/doacao-12.webp",
       alt: "Pessoas se alimentando em um refeitório comunitário.",
     },
   ],
